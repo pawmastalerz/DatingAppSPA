5e7ad6d33cfb483216048ba07a17d1bb68e8ed67
Pawel <paw.mastalerz@gmail.com>
2018-09-21 10:33:01 +0200

    Dynamic main photo selection.

--- src/app/members/member-edit/member-edit.component.ts
+++ src/app/members/member-edit/member-edit.component.ts
@@ -13,6 +13,7 @@ import { NgForm } from '@angular/forms';
 })
 export class MemberEditComponent implements OnInit {
   user: User;
+  photoUrl: string;
 
   @ViewChild('editForm')
   editForm: NgForm;
@@ -35,6 +36,7 @@ export class MemberEditComponent implements OnInit {
     this.route.data.subscribe(data => {
       this.user = data['user'];
     });
+    this.authService.currentPhotoUrl.subscribe(photoUrl => this.photoUrl = photoUrl);
   }
 
   updateUser() {
@@ -46,5 +48,9 @@ export class MemberEditComponent implements OnInit {
       }, error => {
         this.alertify.error(error);
       });
+  }
+
+  updateMainPhoto(photoUrl) {
+    this.user.photoUrl = photoUrl;
   }
 }
